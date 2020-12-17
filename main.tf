84004324d0e4a4109f6f1957b74f716c17cc77a9
Vinod Kumar Kashi Venkatesh Prasad <vinvinod@publicisgroupe.net>
2020-12-17 11:03:14 +0530

    second commit

--- main.tf
+++ main.tf
@@ -1,15 +1,14 @@
 
-
-# terraform {
-#   backend "s3" {
-#     bucket = "vinods3tsstate"
-#     key = "dev"
-#      region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "vinods3tsstate"
+    key = "dev"
+     region = "us-east-1"
+  }
+}
 
 module "training" {
-   source  = "vinodkumarkv/trainingEC2/aws"
+  source  = "vinodkumarkv/trainingEC2/aws"
   version = "0.0.1"
   # source        = "./module/ec2"
   ami           = "ami-04d29b6f966df1537"
